c8de7022aeb414bb3ccef073c1332d8285cece64
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-08-07 18:57:52 -0700

    feat: expose compiler analytics context

--- packages/engine/src/analytics/index.ts
+++ packages/engine/src/analytics/index.ts
@@ -11,3 +11,13 @@ export { getDashboardMetrics } from "./getDashboardMetrics.js";
 export { getScanInsights } from "./analyticsService.js";
 
 export { getAssetLiveMetrics } from "./dashboardAnalytics.js";
+
+export {
+  createCompilerAnalyticsContext,
+  mergeCompilerAnalytics,
+} from "./compilerAnalytics.js";
+
+export type {
+  CompilerAnalyticsEvent,
+  CompilerAnalyticsContext,
+} from "./compilerAnalytics.js";
