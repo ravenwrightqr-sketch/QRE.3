0da8947a6a65d701197fa7b28fa801885e84390b
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-08-13 22:44:35 -0700

    migrate web compiler types to ExperienceMoment

--- apps/web/src/types/compiler.ts
+++ apps/web/src/types/compiler.ts
@@ -3,40 +3,22 @@ import type {
   ExperienceWorld,
   ExperienceBlueprint,
   FlowStep,
-  Moment,
+  ExperienceMoment,
   CinematicScene,
   ExperienceModel,
 } from "@qre/contracts";
 
-
-export type ExperienceIntent = {
-
-  prompt:string;
-
-};
-
+export type ExperienceIntent = { prompt: string };
 
 export type CompiledExperience = {
-
-  genome:ExperienceGenome;
-
-  world:ExperienceWorld;
-
-  blueprint:ExperienceBlueprint;
-
-  flowSteps:FlowStep[];
-
-  moments:Moment[];
-
-  cinematicScenes:CinematicScene[];
-
-  model:ExperienceModel;
-
-  title:string;
-
-  estimatedDuration:number;
-
-  momentCount:number;
-
+  genome: ExperienceGenome;
+  world: ExperienceWorld;
+  blueprint: ExperienceBlueprint;
+  flowSteps: FlowStep[];
+  moments: ExperienceMoment[];
+  cinematicScenes: CinematicScene[];
+  model: ExperienceModel;
+  title: string;
+  estimatedDuration: number;
+  momentCount: number;
 };
-
