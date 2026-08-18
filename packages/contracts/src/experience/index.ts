9b2b12e1a14f1db1e42125f6c1aec7e08ecd7b9a
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-08-18 03:23:05 -0700

    geo: export v17 spatial evidence contracts

--- packages/contracts/src/experience/index.ts
+++ packages/contracts/src/experience/index.ts
@@ -32,3 +32,4 @@ export * from "./indexV13.js";
 export * from "./memoryIntelligenceV14.js";
 export * from "./memoryForesightV15.js";
 export * from "./memorySpatialV16.js";
+export * from "./memorySpatialV17.js";
