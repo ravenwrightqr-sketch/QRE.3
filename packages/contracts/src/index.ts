620b558f015b755cc2d7dc89be5c43da6309d0b0
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-08-24 10:22:49 -0700

    feat(reality): export foundational reality authority

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -23,6 +23,7 @@ export * from "./experience/memoryForesightV15.js";
 export * from "./experience/memorySpatialV16.js";
 export type { TheState } from "./theState.js";
 export * from "./authoringIntelligence.js";
+export * from "./realityAuthority.js";
 export type { MediaAsset, MediaType } from "./media.js";
 export type { SponsorPolicy, SponsorPlacement, SponsorFrequency, SponsorSignal } from "./experience/sponsor.js";
 export type { RewardKind, RewardCompliance, SponsorRewardProgram, RewardAttribution, RewardTransaction, RewardBalance, RewardRecommendation } from "./experience/sponsorRewards.js";
