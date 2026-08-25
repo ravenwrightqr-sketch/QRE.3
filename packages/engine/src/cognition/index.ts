5115e6de13dc833e331e5e1e209030d6cc73d1f2
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-08-24 23:21:12 -0700

    fix: export governed analytics classifier

--- packages/engine/src/cognition/index.ts
+++ packages/engine/src/cognition/index.ts
@@ -6,3 +6,8 @@ export type {
   UniversalMindContext,
   UniversalMindResult,
 } from "./universalMind.js";
+
+export {
+  classifyAnalyticsEvent,
+  summarizeCognitiveAnalytics,
+} from "./cognitiveAnalytics.js";
