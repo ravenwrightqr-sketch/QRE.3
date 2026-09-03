8a363095ac4edb75502f5d53c51babe51aadf506
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-09-02 20:16:21 -0700

    delete canonical Mouth compatibility seam

--- apps/api/src/services/authorMouthCandidateSearchCanonical.ts
+++
@@ -1,3 +0,0 @@
-export { buildMouthCandidateMessages, parseMouthCandidateBatch, scoreMouthCandidate, isAuthorizedMouthCandidate, selectBestMouthSequence } from "./authorMouth.js";
-export type { MouthCandidateGenerationInput } from "./authorMouth.js";
-export type { MouthCandidate, MouthCandidateBatch, MouthCandidateBeat, MouthCandidateSelection } from "@qre/contracts";
