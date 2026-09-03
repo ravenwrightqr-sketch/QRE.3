ce4eb43f29181087e1720b23179c79ccbf748628
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-09-02 20:16:17 -0700

    delete legacy Mouth search seam

--- apps/api/src/services/authorMouthCandidateSearch.ts
+++
@@ -1,4 +0,0 @@
-export { buildMouthCandidateMessages, parseMouthCandidateBatch, scoreMouthCandidate, isAuthorizedMouthCandidate, selectBestMouthSequence } from "./authorMouth.js";
-export type { MouthCandidateGenerationInput } from "./authorMouth.js";
-export type { MouthCandidate, MouthCandidateBatch, MouthCandidateBeat, MouthCandidateSelection } from "@qre/contracts";
-export { deriveViewerStateCut } from "./authorViewerStateCut.js";
