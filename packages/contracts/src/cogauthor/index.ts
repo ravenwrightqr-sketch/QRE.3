e9de61ee61381092ec84f64adff23dc7d828bc07
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-09-03 20:17:15 -0700

    feat: surface canonical metamorphic contract

--- packages/contracts/src/cogauthor/index.ts
+++ packages/contracts/src/cogauthor/index.ts
@@ -2,5 +2,6 @@
 export * from "./authorBrain.js";
 export * from "./cognition.js";
 export * from "./latentMovie.js";
+export * from "./metamorphic.js";
 export * from "./realityGraph.js";
 export * from "./mouth.js";
