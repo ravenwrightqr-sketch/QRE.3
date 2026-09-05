e1a4bdb14fb9cd84b6f981a0084015e4b03eb1fb
raven padilla <ravenwrightqr@gmail.com>
2026-09-04 23:30:51 -0700

    restore organized canonical contracts

--- packages/contracts/src/cogauthor/authorBrain.ts
+++
@@ -1,8 +0,0 @@
-/** Compatibility surface. Canonical AuthorBrain contracts live under experience/. */
-export type {
-  AuthorRhythm,
-  AuthorCreativeBrief,
-  AuthorBrainTruth,
-  AuthorScene,
-  AuthorRenderedScene,
-} from "../experience/authorBrain.js";
