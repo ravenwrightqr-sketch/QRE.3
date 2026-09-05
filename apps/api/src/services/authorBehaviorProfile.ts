bd478e62d3517e7422b30e7890e80fb6da25226f
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-09-05 15:52:28 -0700

    compat(experience): alias legacy behavior import

---
+++ apps/api/src/services/authorBehaviorProfile.ts
@@ -0,0 +1,2 @@
+export type { ExperienceBehaviorProfile as AuthorBehaviorProfile } from "./experienceBehavior.js";
+export { buildExperienceBehaviorProfile as buildAuthorBehaviorProfile } from "./experienceBehavior.js";
