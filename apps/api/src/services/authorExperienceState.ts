63be5c08621fc995f8366b9cbcd26b3cbe857014
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-09-05 15:52:20 -0700

    compat(experience): alias legacy state import to experience state

---
+++ apps/api/src/services/authorExperienceState.ts
@@ -0,0 +1,2 @@
+export { buildExperienceState as buildAuthorExperienceState } from "./experienceState.js";
+export type { ExperienceState as AuthorExperienceState, ExperienceTempo as AuthorTempo, ExperienceTempoMode as AuthorTempoMode } from "@qre/contracts";
