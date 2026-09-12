c0e3aa7c909ee352d2e34e7df15bb333773fa530
ravenwrightqr-sketch <ravenwrightqr@gmail.com>
2026-09-11 22:20:07 -0700

    author: remove obsolete experience state shim

--- apps/api/src/services/authorExperienceState.ts
+++
@@ -1,2 +0,0 @@
-export { buildExperienceState as buildAuthorExperienceState } from "./experienceState.js";
-export type { ExperienceState as AuthorExperienceState, ExperienceTempo as AuthorTempo, ExperienceTempoMode as AuthorTempoMode } from "@qre/contracts";
